848d196104ab918ec261716066e4ecef5cef7b18
Mais serhan <159805997+MaisSerhan@users.noreply.github.com>
2025-03-13 05:14:29 +0200

    Update main.js

--- Assets/scripts/main.js
+++ Assets/scripts/main.js
@@ -1,4 +1,13 @@
-
+// Function to load the HTML content from an external file into a container
+function loadHTML(url, containerId) {
+  fetch(url)
+      .then(response => response.text())
+      .then(data => {
+          document.getElementById(containerId).innerHTML = data;
+          updateUI('ar', 'العربية'); // Load English translations
+      })
+      .catch(error => console.error('Error loading the HTML file:', error));
+}
 //for scroll up and down
 let lastScrollTop = 0;
 const header = document.querySelector('header');
