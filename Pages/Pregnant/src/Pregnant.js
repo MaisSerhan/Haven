2232a0987201de5d82743cb43514872c57307099
Mais serhan <159805997+MaisSerhan@users.noreply.github.com>
2025-03-13 05:15:16 +0200

    Update Pregnant.js

--- Pages/Pregnant/src/Pregnant.js
+++ Pages/Pregnant/src/Pregnant.js
@@ -1,14 +1,3 @@
-// Function to load the HTML content from an external file into a container
-function loadHTML(url, containerId) {
-  fetch(url)
-      .then(response => response.text())
-      .then(data => {
-          document.getElementById(containerId).innerHTML = data;
-          updateUI('ar', 'العربية'); // Load English translations
-      })
-      .catch(error => console.error('Error loading the HTML file:', error));
-}
-
 document.addEventListener('DOMContentLoaded', function() {
   loadHTML('../../Assets/Html/navbar.html', 'navbar-container');
   loadHTML('../../Assets/Html/footer.html', 'footer-container');
