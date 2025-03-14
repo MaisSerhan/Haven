8cc444ce11848a9a8646b64af408c423361d8932
Mais serhan <159805997+MaisSerhan@users.noreply.github.com>
2025-03-14 08:39:10 +0200

    Update home.js

--- Pages/Home/src/home.js
+++ Pages/Home/src/home.js
@@ -1,10 +1,20 @@
+// Function to load the HTML content from an external file into a container
+function loadHTML(url, containerId) {
+  fetch(url)
+      .then(response => response.text())
+      .then(data => {
+          document.getElementById(containerId).innerHTML = data;
+          updateUI('ar', 'العربية'); // Load English translations
+          updatePost('ar', 'العربية'); // Load English translations
+      })
+      .catch(error => console.error('Error loading the HTML file:', error));
+}
 
 document.addEventListener('DOMContentLoaded', function() {
   loadHTML('../../Assets/Html/navbar.html', 'navbar-container');
   loadHTML('../../Assets/Html/footer.html', 'footer-container');
   
 });
-
 async function updatePost(lang, name) {
   console.log(1)
   const language = document.getElementById('language');
