87763fe0d6f49b34a2090aacbd0c3b1665e708be
Mais serhan <159805997+MaisSerhan@users.noreply.github.com>
2025-03-18 13:45:57 +0200

    Update and rename home.js to Birth.js

---
+++ Pages/Birth/src/Birth.js
@@ -0,0 +1,29 @@
+// Function to load the HTML content from an external file into a container
+function loadHTML(url, containerId, callback) {
+  fetch(url)
+      .then(response => response.text())
+      .then(data => {
+          document.getElementById(containerId).innerHTML = data;
+          if (callback) callback(); // Ensure updates happen after loading
+      })
+      .catch(error => console.error('Error loading the HTML file:', error));
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  let savedLang = localStorage.getItem('selectedLang') || 'ar'; // Default to Arabic
+  let langName = savedLang === 'ar' ? 'العربية' : 'English';
+
+  loadHTML('../../Assets/Html/navbar.html', 'navbar-container', () => {
+      updateUI(savedLang, langName);
+  });
+
+  loadHTML('../../Assets/Html/footer.html', 'footer-container', () => {
+    updateUI(savedLang, langName);
+  });
+
+  updatePost(savedLang, langName);
+});
+
+// Update Post Content Translation
+async function updatePost(lang, name) {
+}
