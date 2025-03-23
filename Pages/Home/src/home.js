627490c7bce7d7ee7228758683abf394c45591d8
Mais serhan <159805997+MaisSerhan@users.noreply.github.com>
2025-03-23 10:36:24 +0200

    Update home.js

--- Pages/Home/src/home.js
+++ Pages/Home/src/home.js
@@ -12,74 +12,13 @@ function loadHTML(url, containerId, callback) {
 
 // Handle Page Load & Language Persistence
 document.addEventListener('DOMContentLoaded', function () {
- /* let savedLang = localStorage.getItem('selectedLang') || 'ar'; // Default to Arabic
-  let langName = savedLang === 'ar' ? 'العربية' : 'English';*/
+ 
 
   loadHTML('../../Assets/Html/navbar.html', 'navbar-container', () => {
-     // updateUI(savedLang, langName);
+    
   });
 
   loadHTML('../../Assets/Html/footer.html', 'footer-container', () => {
-   // updateUI(savedLang, langName);
+   
   });
 });
-
-// Update Post Content Translation
-/*async function updatePost(lang, name) {
-  const language = document.getElementById('language');
-  language.textContent = name;
-
-  const translations = {
-      "i18n-a-Team": "Post.a-Team",
-      "i18n-a-PTUK": "Post.a-PTUK",
-      "i18n-a-Haven-Vision": "Post.a-Haven-Vision",
-      "i18n-a-Haven-name": "Post.a-Haven-name",
-      "i18n-p-Team": "Post.p-Team",
-      "i18n-p-PTUK": "Post.p-PTUK",
-      "i18n-p-Haven-Vision": "Post.p-Haven-Vision",
-      "i18n-p-Haven-name": "Post.p-Haven-name",
-      'i18n-about-us': 'Post.about-us',
-      'i18n-p-about': 'Post.p-about',
-      'i18n-a-post1': 'Post.a-post1',
-      'i18n-p-post1': 'Post.p-post1',
-      'i18n-d-post1': 'Post.d-post1',
-      'i18n-readmore': 'Post.readmore',
-      'i18n-a-post2': 'Post.a-post2',
-      'i18n-p-post2': 'Post.p-post2',
-      'i18n-d-post2': 'Post.d-post2',
-      'i18n-a-post3': 'Post.a-post3',
-      'i18n-p-post3': 'Post.p-post3',
-      'i18n-d-post3': 'Post.d-post3',
-      'i18n-a-post4': 'Post.a-post4',
-      'i18n-p-post4': 'Post.p-post4',
-      'i18n-d-post4': 'Post.d-post4',
-      'i18n-a-post5': 'Post.a-post5',
-      'i18n-p-post5': 'Post.p-post5',
-      'i18n-d-post5': 'Post.d-post5',
-      'i18n-a-post6': 'Post.a-post6',
-      'i18n-p-post6': 'Post.p-post6',
-      'i18n-d-post6': 'Post.d-post6',
-  };
-
-  for (const className in translations) {
-      if (translations.hasOwnProperty(className)) {
-          const translationKey = translations[className];
-          const elements = document.querySelectorAll('.' + className);
-
-          await Promise.all(Array.from(elements).map(async (element) => {
-              element.textContent = await translate(lang, translationKey);
-          }));
-      }
-  }
-
-  let about = document.querySelector('.i18n-p-about');
-  if (lang === 'ar') {
-      about.classList.remove("text-start");
-      about.classList.add("text-end");
-      about.dir = "rtl";
-  } else {
-      about.classList.remove("text-end");
-      about.classList.add("text-start");
-      about.dir = "ltr";
-  }
-}*/
