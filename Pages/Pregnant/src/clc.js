671c69743da9eca7751b75ac8a36bfb06ce56439
Mais serhan <159805997+MaisSerhan@users.noreply.github.com>
2025-04-08 10:19:23 +0200

    Create clc.js

---
+++ Pages/Pregnant/src/clc.js
@@ -0,0 +1,12 @@
+const iframe = document.getElementById("myIframe");
+const suss=document.querySelector('.bg-success');
+console.log(iframe ,suss)
+iframe.onload = function() {console.log(iframe ,suss)
+    const iframeDoc = iframe.contentDocument || iframe.contentWindow.document;
+    iframeDoc.body.style.backgroundColor = "rgb(255, 171, 200)";
+    suss.style.backgroundColor="rgb(255 171 200) !important" ;
+};
+document.addEventListener('DOMContentLoaded', function () {
+  loadHTML('../../Assets/Html/navbar.html', 'navbar-container', () => {
+    
+  });
