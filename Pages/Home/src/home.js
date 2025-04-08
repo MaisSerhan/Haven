08f316f30f6310b877d6c28be37eabd438879bab
Mais serhan <159805997+MaisSerhan@users.noreply.github.com>
2025-04-08 09:46:07 +0200

    Update home.js

--- Pages/Home/src/home.js
+++ Pages/Home/src/home.js
@@ -1,26 +1,9 @@
-
-// Load External HTML (Navbar, Footer)
-function loadHTML(url, containerId, callback) {
-  fetch(url)
-      .then(response => response.text())
-      .then(data => {
-          document.getElementById(containerId).innerHTML = data;
-          if (callback) callback();
-          navbar();
-      })
-      .catch(error => console.error('Error loading the HTML file:', error));
-}
-
 // Handle Page Load & Language Persistence
 document.addEventListener('DOMContentLoaded', function () {
- 
-
   loadHTML('../../Assets/Html/navbar.html', 'navbar-container', () => {
     
   });
-
   loadHTML('../../Assets/Html/footer.html', 'footer-container', () => {
-   
   });
 });
 
