cd79ed8c1ffe93a4d76dae41e51945040de38381
Mais serhan <159805997+MaisSerhan@users.noreply.github.com>
2025-04-08 10:33:11 +0200

    Update clc.js

--- Pages/Pregnant/src/clc.js
+++ Pages/Pregnant/src/clc.js
@@ -1,13 +1,9 @@
-document.addEventListener('DOMContentLoaded', function () {
-  loadHTML('../../Assets/Html/navbar.html', 'navbar-container', () => {
- const iframe = document.getElementById("myIframe");
-const suss=document.querySelector('.bg-success');
-console.log(iframe ,suss)
-iframe.onload = function() {console.log(iframe ,suss)
-    const iframeDoc = iframe.contentDocument || iframe.contentWindow.document;
-    iframeDoc.body.style.backgroundColor = "rgb(255, 171, 200)";
-    suss.style.backgroundColor="rgb(255 171 200) !important" ;
-});   
+  document.addEventListener('DOMContentLoaded', function() {
+  
+    loadHTML('../../Assets/Html/navbar.html', 'navbar-container', () => {
+     //   updateUI(savedLang, langName);
+    });
+  //updatePost(savedLang, langName);
 });
 
 
